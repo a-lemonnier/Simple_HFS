83e671503c009a18a8d845c5055d8665f779fb00
some_stellar_tools <40183590+a-lemonnier@users.noreply.github.com>
2018-07-20 19:21:21 +0200

    lambda function fixed

--- include/cgwr.cpp
+++ include/cgwr.cpp
@@ -183,7 +183,7 @@ double long _CGWR::W6j(void) {
 //Selection rules //TODO ?
 // j_i has to obey to triangular delta below
 /*********************************************************************/
-    bool delta = [] (double long a, double long b, double long c) {
+    auto delta = [] (double long a, double long b, double long c) {
         return (abs(a-b)<=c)&&(c<=a+b);
     };
     
